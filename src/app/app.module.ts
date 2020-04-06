b269129c4f5bd49e11f7eb4dd7028252145e3010
viswanathreddyvangala <v.viswa000@gmail.com>
2020-04-06 12:23:37 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store'; 
+
+import { ShoppingReducer } from './store/reducers/shopping.reducer';
+import {ShoppingEffects} from './store/effects/shopping.effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { HttpClientModule } from '@angular/common/http';
+import { EffectsModule } from '@ngrx/effects';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatIconModule} from '@angular/material/icon';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    StoreModule.forRoot({
+     shopping: ShoppingReducer
+   }),
+   EffectsModule.forRoot([ShoppingEffects]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
